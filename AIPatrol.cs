d31d59d5b0c519765cf12098896b821e1c1de210
isngrim <jeff.sinclair343@gmail.com>
2019-06-09 10:45:31 -0700

    Add files via upload

---
+++ AIPatrol.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AIPatrol : MonoBehaviour
+{
+    [SerializeField]
+    bool patrolWaiting;
+    [SerializeField]
+    float totalWaitTime = .2f;
+    [SerializeField]
+    float switchProbability = 0.2f;
+    NavMeshAgent navMeshAgent;
+   public ConnectedWaypoint currentWaypoint;
+    ConnectedWaypoint previousWaypoint;
+    
+    bool travelling;
+    bool waiting;
+    float waitTimer;
+    int waypointsVisited;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        StartPatrol();
+    }
+    private void StartPatrol()
+    {
+       
+
+        Debug.Log("Starting Patrol");
+
+        navMeshAgent.updateRotation = true;
+        if (navMeshAgent == null)
+        {
+            Debug.LogError("Missing NavMeshAgent on " + gameObject.name);
+        }
+        else
+        {
+            if (currentWaypoint == null)
+            {
+                GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+                if (allWaypoints.Length > 0)
+                {
+                    while (currentWaypoint == null)
+                    {
+                        int random = Random.Range(0, allWaypoints.Length);
+                        ConnectedWaypoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWaypoint>();
+                        if (startingWaypoint != null)
+                        {
+                            currentWaypoint = startingWaypoint;
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Failed to Find Waypoints to use in the scene");
+                }
+
+
+
+
+            }
+            
+            SetDestination();
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+            Debug.Log("Im Patrolling!");
+            if (travelling && navMeshAgent.remainingDistance <= 1.0f)
+            {
+                Debug.Log("travilng = false");
+                travelling = false;
+                waypointsVisited++;
+                if (patrolWaiting)
+                {
+                    waiting = true;
+                    waitTimer = 0f;
+                }
+                else
+                {
+                    Debug.Log("start setting destination");
+                    SetDestination();
+                }
+            }
+            if (waiting)
+            {
+                waitTimer += Time.deltaTime;
+                if (waitTimer >= totalWaitTime)
+                {
+                    waiting = false;
+
+                    SetDestination();
+                }
+            }
+        
+       
+    }
+    private void SetDestination()
+    {
+        Debug.Log("Setting Destination!");
+        if (waypointsVisited > 0)
+        {
+            ConnectedWaypoint nextWaypoint = currentWaypoint.NextWaypoint(previousWaypoint);
+            previousWaypoint = currentWaypoint;
+            currentWaypoint = nextWaypoint;
+        }
+        
+            Vector3 targetVector = currentWaypoint.transform.position;
+
+            navMeshAgent.SetDestination(targetVector);
+        Debug.Log("travilng = true");
+        travelling = true;
+        
+    }
+   
+}
